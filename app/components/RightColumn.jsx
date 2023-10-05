54e4eb3386037a79466b7d91501c8bb80b899467
Getulio Gutemberg <getulio.dev@gmail.com>
2023-10-05 01:27:02 -0300

    Estilizando RightColumn:

--- app/components/RightColumn.jsx
+++ app/components/RightColumn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RightColumn = () => {
   return (
-    <div>
+    <div className='md:mt-20 flex justify-between md:w-[35%] bg-gray-300 min-h-[45%] p-4'>
         <p>Col-2 Start</p>
         <p>Col-2 End</p>
     </div>
