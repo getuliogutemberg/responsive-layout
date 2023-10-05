5d3cfcd02192f32b6fe5d6c2701c2d80ad0509d7
Getulio Gutemberg <getulio.dev@gmail.com>
2023-10-05 01:29:21 -0300

    Importando LeftColumn e RightColumn:

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import SideNav from './components/SideNav'
 import Header from './components/Header.jsx'
+import LeftColmun from './components/LeftColumn.jsx'
+import RigthColumn from './components/RightColumn.jsx'
 export default function Home() {
   return (
     <div className='flex min-h-screen text-black'>
       <SideNav/>
-      <div>
+      <div className='flex-1 md:flex h-screen relative '>
 				<Header/>
+        <LeftColmun/>
+        <RigthColumn/>
 			</div>
     </div>
   )
