227d4851d86d21f329fc0c43a3c6cf24011a6b0a
Getulio Gutemberg <getulio.dev@gmail.com>
2023-10-05 01:26:23 -0300

    Estilizando LeftColumn:

--- app/components/LeftColumn.jsx
+++ app/components/LeftColumn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LeftColumn = () => {
   return (
-    <div>
+    <div className='mt-20 flex justify-between md:w-[65%] bg-white min-h-[45%] p-4'>
         <p>Col-1 Start</p>
         <p>Col-1 End</p>
     </div>
