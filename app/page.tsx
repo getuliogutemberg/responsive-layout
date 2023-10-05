4c6a15b92d57bb3a9b6ebdcbad85ac18e2000663
Getulio Gutemberg <getulio.dev@gmail.com>
2023-10-05 01:25:00 -0300

    Importando o Header:

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import SideNav from './components/SideNav'
-
+import Header from './components/Header.jsx'
 export default function Home() {
   return (
     <div className='flex min-h-screen text-black'>
       <SideNav/>
+      <div>
+				<Header/>
+			</div>
     </div>
   )
 }
