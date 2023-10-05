5cc5d55b1058a702684f32e2fa67bcc8f19e16a1
Getulio Gutemberg <getulio.dev@gmail.com>
2023-10-05 01:23:06 -0300

    Importando SideNav:

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import SideNav from './components/SideNav'
 
 export default function Home() {
   return (
     <div className='flex min-h-screen text-black'>
-
+      <SideNav/>
     </div>
   )
 }
