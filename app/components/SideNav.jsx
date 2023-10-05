10702de94f5bd4b1814891d49bd496577e1063a0
Getulio Gutemberg <getulio.dev@gmail.com>
2023-10-05 01:22:07 -0300

    Codando SideNav:

--- app/components/SideNav.jsx
+++ app/components/SideNav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const SideNav = () => {
-  return (
-    <div>SideNav</div>
-  )
+    return (
+        <div >
+        <div>
+          <p>Nav Start</p>
+          <ul>
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+          </ul>
+
+        </div>
+        <p>Nav end</p>
+        </div>
+    )
 }
 
 export default SideNav
