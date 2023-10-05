68ccad2c46bed241ff19d12403b2f1238260fcb9
Getulio Gutemberg <getulio.dev@gmail.com>
2023-10-05 01:25:26 -0300

    Estilizando o Header:

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>
+    <div className='bg-red-400 absolute h-20 w-full flex justify-between items-center p-4'>
         <p>Header Start</p>
         <p>Header End</p>
     </div>
